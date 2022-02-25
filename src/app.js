f93936a8156644ecc76bf5f3aeb58089c0f8259c
shaikhalam1996 <83396406+shaikhalam1996@users.noreply.github.com>
2022-02-25 16:48:47 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-//require("./db/conn");
+require("./db/conn");
 const path = require("path");
 const hbs = require("hbs");
 const Userdata = require("./models/user");
